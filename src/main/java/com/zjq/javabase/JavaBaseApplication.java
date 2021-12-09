93850b312977408f04abe8d4104cb90f939b794e
zhanjq <1187625098@qq.com>
2021-12-09 15:28:34 +0800

    项目初始化

---
+++ src/main/java/com/zjq/javabase/JavaBaseApplication.java
@@ -0,0 +1,18 @@
+package com.zjq.javabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * JAVA基础
+ * @author zjq
+ */
+@SpringBootApplication
+public class JavaBaseApplication extends WebMvcConfigurationSupport {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBaseApplication.class, args);
+    }
+
+}
