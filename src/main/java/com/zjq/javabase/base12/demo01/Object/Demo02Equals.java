f53ca0eecd8f2c2eee2a6c1f864e1cd630eaa5c4
zhanjq <1187625098@qq.com>
2021-12-16 09:53:10 +0800

    Object类、常用API

---
+++ src/main/java/com/zjq/javabase/base12/demo01/Object/Demo02Equals.java
@@ -0,0 +1,52 @@
+package com.zjq.javabase.base12.demo01.Object;
+
+import java.util.ArrayList;
+
+/**
+ * * `public boolean equals(Object obj)`：指示其他某个对象是否与此对象“相等”。
+ *
+ * 调用成员方法equals并指定参数为另一个对象，则可以判断这两个对象是否是相同的。
+ * 这里的“相同”有默认和自定义两种方式。
+ *
+ * ### 默认地址比较
+ *
+ * 如果没有覆盖重写equals方法，那么Object类中默认进行`==`运算符的对象地址比较，
+ * 只要不是同一个对象，结果必然为false。
+ *
+ * ### 对象内容比较
+ *
+ * 如果希望进行对象的内容比较，即所有或指定的部分成员变量相同就判定两个对象相同，
+ * 则可以覆盖重写equals方法。
+ * @author zjq
+ */
+public class Demo02Equals {
+    public static void main(String[] args) {
+        /*
+            Person类默认继承了Object类,所以可以使用Object类的equals方法
+            boolean equals(Object obj) 指示其他某个对象是否与此对象“相等”。
+            equals方法源码:
+                public boolean equals(Object obj) {
+                    return (this == obj);
+                }
+                参数:
+                    Object obj:可以传递任意的对象
+                    == 比较运算符,返回的是一个布尔值 true false
+                    基本数据类型:比较的是值
+                    引用数据类型:比价的是两个对象的地址值
+               this是谁?那个对象调用的方法,方法中的this就是那个对象;p1调用的equals方法所以this就是p1
+               obj是谁?传递过来的参数p2
+               this==obj -->p1==p2
+         */
+        Person p1 = new Person("迪丽热巴",18);
+        //Person p2 = new Person("古力娜扎",19);
+        Person p2 = new Person("迪丽热巴",18);
+        System.out.println("p1:"+p1);//p1:com.zjq.javabase.base12.demo01.Object.Person@58ceff1
+        System.out.println("p2:"+p2);//p2:com.zjq.javabase.base12.demo01.Object.Person@7c30a502
+
+        //p1=p2;//把p2的地址值赋值给p1
+        ArrayList<String> list = new ArrayList<>();
+
+        boolean b = p1.equals(p1);
+        System.out.println(b);
+    }
+}
---
+++ src/main/java/com/zjq/javabase/base12/demo01/Object/Demo03Objects.java
@@ -0,0 +1,43 @@
+package com.zjq.javabase.base12.demo01.Object;
+
+import java.util.Objects;
+
+/**
+ * `java.lang.Object`类是Java语言中的根类，即所有类的父类。它中描述的所有方法子类都可以使用。在对象实例化的时候，最终找的父类就是Object。
+ *
+ * 如果一个类没有特别指定父类，	那么默认则继承自Object类。例如：
+ *
+*
+*根据JDK源代码及Object类的API文档，Object类当中包含的方法有11个。今天我们主要学习其中的2个：
+*
+** `public String toString()`：返回该对象的字符串表示。
+** `public boolean equals(Object obj)`：指示其他某个对象是否与此对象“相等”。
+ *
+ * 在**JDK7**添加了一个Objects工具类，它提供了一些方法来操作对象，它由一些静态的实用
+ * 方法组成，这些方法是null-save（空指针安全的）或null-tolerant（容忍空指针的），用于
+ * 计算对象的hashcode、返回对象的字符串表示形式、比较两个对象。
+ *
+ * 在比较两个对象的时候，Object的equals方法容易抛出空指针异常，而Objects类中的equals方
+ * 法就优化了这个问题。方法如下：
+ *
+ * * `public static boolean equals(Object a, Object b)`:判断两个对象是否相等。
+* @author zjq
+ */
+public class Demo03Objects {
+    public static void main(String[] args) {
+        String s1 = "abc";
+        //String s1 = null;
+        String s2 = "abc";
+        //boolean b = s1.equals(s2); // NullPointerException null是不能调用方法的,会抛出空指针异常
+        //System.out.println(b);
+        /*
+            Objects类的equals方法:对两个对象进行比较,防止空指针异常
+            public static boolean equals(Object a, Object b) {
+                return (a == b) || (a != null && a.equals(b));
+            }
+         */
+        boolean b2 = Objects.equals(s1, s2);
+        System.out.println(b2);
+
+    }
+}
