4696eca9a35d058c1bdcadefd31be03c9e31818f
zhanjq <1187625098@qq.com>
2021-12-17 09:29:24 +0800

    List、Set

---
+++ src/main/java/com/zjq/javabase/base14/demo03/hashCode/Person.java
@@ -0,0 +1,13 @@
+package com.zjq.javabase.base14.demo03.hashCode;
+
+/**
+ * @author zjq
+ */
+public class Person extends  Object{
+    //重写hashCode方法
+
+    @Override
+    public int hashCode() {
+        return  1;
+    }
+}
---
+++ src/main/java/com/zjq/javabase/base14/demo05/Collections/Student.java
@@ -0,0 +1,41 @@
+package com.zjq.javabase.base14.demo05.Collections;
+
+/**
+ * @author zjq
+ */
+public class Student {
+    private String name;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
