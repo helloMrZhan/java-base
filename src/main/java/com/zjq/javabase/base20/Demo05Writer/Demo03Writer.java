812d3974127d0ef1cf82075135f2532d5233be2b
Jack <1187625098@qq.com>
2021-12-23 22:10:24 +0800

    字节流、字符流

---
+++ src/main/java/com/zjq/javabase/base20/Demo05Writer/Demo03Writer.java
@@ -0,0 +1,32 @@
+package com.zjq.javabase.base20.Demo05Writer;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+    字符输出流写数据的其他方法
+        - void write(char[] cbuf)写入字符数组。
+        - abstract  void write(char[] cbuf, int off, int len)写入字符数组的某一部分,off数组的开始索引,len写的字符个数。
+        - void write(String str)写入字符串。
+        - void write(String str, int off, int len) 写入字符串的某一部分,off字符串的开始索引,len写的字符个数。
+ * @author zjq
+ */
+public class Demo03Writer {
+    public static void main(String[] args) throws IOException {
+        FileWriter fw = new FileWriter("09_IOAndProperties\\f.txt");
+        char[] cs = {'a','b','c','d','e'};
+        //void write(char[] cbuf)写入字符数组。
+        fw.write(cs);//abcde
+
+        //void write(char[] cbuf, int off, int len)写入字符数组的某一部分,off数组的开始索引,len写的字符个数。
+        fw.write(cs,1,3);//bcd
+
+        //void write(String str)写入字符串。
+        fw.write("zjq");//zjq
+
+        //void write(String str, int off, int len) 写入字符串的某一部分,off字符串的开始索引,len写的字符个数。
+        fw.write("zjq666",2,3);//程序员
+
+        fw.close();
+    }
+}
