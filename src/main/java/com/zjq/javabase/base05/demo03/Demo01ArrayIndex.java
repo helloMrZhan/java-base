b5991a66f85ee06d238ff417087f35ab05385feb
zhanjq <1187625098@qq.com>
2022-04-13 14:44:23 +0800

    数组

--- src/main/java/com/zjq/javabase/base05/demo03/Demo01ArrayIndex.java
+++ src/main/java/com/zjq/javabase/base05/demo03/Demo01ArrayIndex.java
@@ -15,13 +15,12 @@ public class Demo01ArrayIndex {
 
     public static void main(String[] args) {
         int[] array = {15, 25, 35};
-
         System.out.println(array[0]); //15
         System.out.println(array[1]); // 25
         System.out.println(array[2]); // 35
 
-// 错误写法
-// 并不存在3号元素，所以发生异常
+        // 错误写法
+        // 并不存在3号元素，所以发生异常
         System.out.println(array[3]);
     }
 
