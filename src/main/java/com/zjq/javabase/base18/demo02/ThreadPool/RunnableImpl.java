b823ae429ac825b724df81b9e72991536395b015
Jack <1187625098@qq.com>
2022-04-26 15:36:56 +0800

    Java中的线程池

--- src/main/java/com/zjq/javabase/base18/demo02/ThreadPool/RunnableImpl.java
+++ src/main/java/com/zjq/javabase/base18/demo02/ThreadPool/RunnableImpl.java
@@ -6,6 +6,12 @@ package com.zjq.javabase.base18.demo02.ThreadPool;
 public class RunnableImpl implements Runnable{
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName()+"创建了一个新的线程执行");
+        try {
+            System.out.println(Thread.currentThread().getName() + "创建了一个新的线程开始执行");
+            Thread.sleep(2000);
+            System.out.println(Thread.currentThread().getName() + "创建了一个新的线程执行结束");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
